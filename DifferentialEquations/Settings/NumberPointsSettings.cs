586e40d5b794cb3b3995ac0cf85da2d60dbb72bd
Ilya Eremin <i.eremin@skbkontur.ru>
2021-05-03 12:45:09 +0500

    Copy project

---
+++ DifferentialEquations/Settings/NumberPointsSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace DifferentialEquations.Settings
+{
+    public class NumberPointsSettings
+    {
+        [DisplayName("Колличество точек")]
+        [Category("Точки")]
+        public int NumberPoints { get; set; } = 18;
+    }
+}
---
+++ DifferentialEquations/Settings/SettingsForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DifferentialEquations.Settings
+{
+    public static class SettingsForm
+    {
+        public static SettingsForm<TSettings> For<TSettings>(TSettings settings)
+        {
+            return new SettingsForm<TSettings>(settings);
+        }
+    }
+
+    public class SettingsForm<TSettings> : Form
+    {
+        public SettingsForm(TSettings settings)
+        {
+            Size = new Size(600, 600);
+            BackColor = Color.MediumPurple;
+            var okButton = new Button
+            {
+                Text = "OK",
+                DialogResult = DialogResult.OK,
+                Dock = DockStyle.Bottom
+            };
+            Controls.Add(okButton);
+            Controls.Add(new PropertyGrid
+            {
+                SelectedObject = settings,
+                Dock = DockStyle.Fill
+            });
+            AcceptButton = okButton;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            Text = "Настройки";
+        }
+    }
+}
